7eb419e6cc7402f521e6b8608e93826cfd8f32af
Renaud <r.dharreville@gmail.com>
2020-04-16 18:42:37 +0200

    adding model for company, files and sentences

--- src/webapp/polls/models.py
+++ src/webapp/polls/models.py
@@ -1,12 +1,41 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Company(models.Model):
+
+    class SectorActivity(models.TextChoices):
+        TERTIAIRE = 'Tertiaire', _('Tertiaire')
+
+    name = models.CharField(max_length=50)
+    pdf_name = models.CharField(max_length=20)
+    sector = models.CharField(
+        max_length=50,
+        choices=SectorActivity.choices,
+        # default=SectorActivity.TERTIAIRE
+    )
+    introduction = models.TextField()
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class File(models.Model):
+
+    class FileType(models.TextChoices):
+        DPEF = 'DPEF', _('dpef')
+        RSE = 'RSE', _('responsabilité sociale et environnementale')
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    path = models.CharField(max_length=200)
+    year = models.IntegerField(default=0)
+    type = models.CharField(
+        max_length=4,
+        choices=FileType.choices,
+        # default=FileType.DPEF
+    )
+
+
+class Sentence(models.Model):
+
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    sentence = models.TextField()
+    # ...
+    # put metadata fields here !
