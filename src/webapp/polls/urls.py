0961bdd5f2ca95fe5d4a8f089a914b1beb5a84c7
Renaud <r.dharreville@gmail.com>
2020-04-17 17:46:36 +0200

    continue adding primary views

--- src/webapp/polls/urls.py
+++ src/webapp/polls/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     # ex: /polls/company/
     path('company/', views.companies, name='companies'),
     # ex: /polls/company/5/
     path('company/<int:company_id>/', views.company, name='company'),
+    path('company/<int:company_id>/set/', views.set_file, name='set_file')
 ]
 
--- src/webapp/polls/views.py
+++ src/webapp/polls/views.py
@@ -1,7 +1,8 @@
 from .models import Company, File, Sentence
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
@@ -19,7 +20,24 @@ def companies(request):
     return HttpResponse(f"Here are the company registered in the database : "
                         f"{[str(c) for c in Company.objects.all()]}.")
 
-# def set_file(request, )
+
+def set_file(request, company_id):
+    company = get_object_or_404(Company, pk=company_id)
+    try:
+        selected_choice = company.file_set.get(pk=request.POST['file'])
+    except (KeyError, File.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/company_forms.html', {
+            'company': company,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # selected_choice.votes += 1
+        # selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:company', args=(company.id,)))
 
 
 def company(request, company_id):
