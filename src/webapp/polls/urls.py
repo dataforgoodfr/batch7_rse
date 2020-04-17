64f244b03eb111169625d23801872820d9fed6cd
Renaud <r.dharreville@gmail.com>
2020-04-17 16:57:22 +0200

    starting adding primary views

--- src/webapp/polls/urls.py
+++ src/webapp/polls/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /polls/company/
+    path('company/', views.companies, name='companies'),
+    # ex: /polls/company/5/
+    path('company/<int:company_id>/', views.company, name='company'),
 ]
 
--- src/webapp/polls/views.py
+++ src/webapp/polls/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from .models import Company, File, Sentence
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-# Create your views here.
+    company_list = Company.objects.all()
+    context = {
+        'company_list': company_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def companies(request):
+    """
+    Return the whole list of companies.
+    """
+    return HttpResponse(f"Here are the company registered in the database : "
+                        f"{[str(c) for c in Company.objects.all()]}.")
+
+# def set_file(request, )
+
+
+def company(request, company_id):
+    """
+    Return the company corresponding to the id passed in argument
+    """
+    company = get_object_or_404(Company, pk=company_id)
+    context = {
+        'company': company
+    }
+    return render(request, 'polls/company.html', context)
+
+
+
