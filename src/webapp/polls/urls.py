331a373d8efc4f20023eb9ea48b1141f007b73c8
Renaud <r.dharreville@gmail.com>
2020-04-18 11:17:24 +0200

    finished part 4 of the tutorial.

--- src/webapp/polls/urls.py
+++ src/webapp/polls/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/company/
     path('company/', views.companies, name='companies'),
     # ex: /polls/company/5/
-    path('company/<int:company_id>/', views.company, name='company'),
+    path('company/<int:pk>/', views.CompanyView.as_view(), name='company'),
     path('company/<int:company_id>/set/', views.set_file, name='set_file')
 ]
 
--- src/webapp/polls/views.py
+++ src/webapp/polls/views.py
@@ -3,22 +3,24 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    company_list = Company.objects.all()
-    context = {
-        'company_list': company_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'company_list'
+
+    def get_queryset(self):
+        return Company.objects.all()
 
 
 def companies(request):
-    """
-    Return the whole list of companies.
-    """
-    return HttpResponse(f"Here are the company registered in the database : "
-                        f"{[str(c) for c in Company.objects.all()]}.")
+    return HttpResponseRedirect(reverse('polls:index'))
+
+
+class CompanyView(generic.DetailView):
+    model = Company
+    # template_name = 'polls/company_detail.html'
 
 
 def set_file(request, company_id):
@@ -40,15 +42,7 @@ def set_file(request, company_id):
         return HttpResponseRedirect(reverse('polls:company', args=(company.id,)))
 
 
-def company(request, company_id):
-    """
-    Return the company corresponding to the id passed in argument
-    """
-    company = get_object_or_404(Company, pk=company_id)
-    context = {
-        'company': company
-    }
-    return render(request, 'polls/company.html', context)
+
 
 
 
