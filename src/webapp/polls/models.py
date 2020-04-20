88a84d274d3c938af568608f38eb3f0cd7730cf1
Renaud <r.dharreville@gmail.com>
2020-04-20 17:28:04 +0200

    adding page fields and constraints on them

--- src/webapp/polls/models.py
+++ src/webapp/polls/models.py
@@ -1,4 +1,5 @@
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 from django.db import models
 from datetime import date
 
@@ -46,21 +47,41 @@ class File(models.Model):
         choices=FileType.choices,
         # default=FileType.DPEF
     )
-
-    def __str__(self):
-        return self.file_object.name  # file path
+    start_rse_page = models.PositiveIntegerField()
+    end_rse_page = models.PositiveIntegerField()
 
     @property
     def year(self):
         return self.date.year
+
+    def clean(self):
+        if self.start_rse_page > self.end_rse_page:
+            raise ValidationError(_("start page must be before end page !"))
+
+    def __str__(self):
+        return self.file_object.name  # file path
 
 
 class Sentence(models.Model):
 
     file = models.ForeignKey(File, on_delete=models.CASCADE)
     sentence = models.TextField()
+    startfilepage = models.PositiveIntegerField()
+    endfilepage = models.PositiveIntegerField()
+    is_RSE_sentence = models.BooleanField(default=False)
     # ...
     # put metadata fields here !
+
+    def _set_rse(self):
+        if self.startfilepage >= self.file.start_rse_page and self.endfilepage <= self.file.end_rse_page:
+            self.is_RSE_sentence = True
+        else:
+            self.is_RSE_sentence = False
+
+    def clean(self):
+        if self.startfilepage > self.endfilepage:
+            raise ValidationError(_("start page must be before end page !"))
+        self._set_rse()
 
     def __str__(self):
         return self.sentence
