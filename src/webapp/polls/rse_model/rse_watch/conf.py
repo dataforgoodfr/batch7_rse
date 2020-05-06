d12ac95da376be0457d9aec4f8ae6d8b4d300777
Charles Gaydon <charles.gaydon@gmail.com>
2020-05-06 16:01:50 +0200

    Changed DebugConfig to have relative paths

--- src/webapp/polls/rse_model/rse_watch/conf.py
+++ src/webapp/polls/rse_model/rse_watch/conf.py
@@ -15,7 +15,7 @@ class Config:
 class DebugConfig:
     dpef_dir = "../data/input/DPEFs/Energéticien/"
     annotations_file = "../data/input/Entreprises/entreprises_rse_annotations.csv"
-    parsed_par_file = "../polls/rse_model/data/processed/DPEFs/dpef_paragraphs_debug.csv"
-    parsed_sent_file = "../polls/rse_model/data/processed/DPEFs/dpef_paragraphs_sentences_debug.csv"
-    model_dir = "../polls/rse_model/data/model_debug/"
+    parsed_par_file = "../data/processed/DPEFs/dpef_paragraphs_debug.csv"
+    parsed_sent_file = "../data/processed/DPEFs/dpef_paragraphs_sentences_debug.csv"
+    model_dir = "../data/model_debug/"
     scorer_pickle_file = model_dir + "vectorizer_component/words_scorer.pckl"
