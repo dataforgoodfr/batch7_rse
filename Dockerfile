6814a5bcafad761e1af45b1e4ab4b03dc10c0b1c
Renaud <r.dharreville@gmail.com>
2020-05-07 12:26:07 +0200

    Move webapp directory, corresponding to #5 issue

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ COPY requirements.txt ./
 
 RUN pip install -r requirements.txt
 
-COPY src/webapp/ .
+COPY webapp .
 
 EXPOSE 8000
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- webapp/polls/migrations/0001_initial.py
+++ webapp/polls/migrations/0001_initial.py
@@ -34,7 +34,8 @@ class Migration(migrations.Migration):
             name='DPEF',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('file_object', models.FileField(help_text='Document DPEF ou DDR au format PDF.', unique=True, upload_to='', validators=[polls.models._validate_file_extension], verbose_name='Fichier PDF')),
+                ('file_object', models.FileField(help_text='Document DPEF ou DDR au format PDF.', unique=True, upload_to='', validators=[
+                    polls.models._validate_file_extension], verbose_name='Fichier PDF')),
                 ('year', models.IntegerField(choices=[(1990, 1990), (1991, 1991), (1992, 1992), (1993, 1993), (1994, 1994), (1995, 1995), (1996, 1996), (1997, 1997), (1998, 1998), (1999, 1999), (2000, 2000), (2001, 2001), (2002, 2002), (2003, 2003), (2004, 2004), (2005, 2005), (2006, 2006), (2007, 2007), (2008, 2008), (2009, 2009), (2010, 2010), (2011, 2011), (2012, 2012), (2013, 2013), (2014, 2014), (2015, 2015), (2016, 2016), (2017, 2017), (2018, 2018), (2019, 2019), (2020, 2020)], help_text='Année de référence du document DPEF', verbose_name='Année')),
                 ('company', models.ForeignKey(help_text="L'entreprise référencée par le document.", on_delete=django.db.models.deletion.CASCADE, to='polls.Company', verbose_name='Entreprise')),
             ],
