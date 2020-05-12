8f46cb2d9376f12f925352f1f8589923bac0b58e
marine <marine.iuso@gmail.com>
2020-05-12 10:35:03 +0200

    design 1st commit

---
+++ webapp/polls/templatetags/filters.py
@@ -0,0 +1,17 @@
+from django.template import Library
+
+register = Library()
+
+@register.filter(name='add_attr')
+def add_attr(field, css):
+    attrs = {}
+    definition = css.split(',')
+
+    for d in definition:
+        if ':' not in d:
+            attrs['class'] = d
+        else:
+            key, val = d.split(':')
+            attrs[key] = val
+
+    return field.as_widget(attrs=attrs)
