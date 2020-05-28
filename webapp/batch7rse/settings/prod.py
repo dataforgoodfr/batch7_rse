7cd05bcfb6be064ceb0b925c6f03bc13c45fdea3
Charles Gaydon <charles.gaydon@gmail.com>
2020-05-28 18:26:11 +0200

    Added parallel computing for parsing; problem of locked db still exists

--- webapp/batch7rse/settings/prod.py
+++ webapp/batch7rse/settings/prod.py
@@ -13,5 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 25, # in seconds
+        }
     }
 }
--- webapp/polls/rse_model/rse_watch/pdf_parser.py
+++ webapp/polls/rse_model/rse_watch/pdf_parser.py
@@ -475,10 +475,10 @@ def get_sentences_from_all_pdfs(config):
     Parses all dpefs into a sentence-level format and save the resulting csv according to config.
     """
     companies_metadata_dict = get_companies_metadata_dict(config)
-    all_input_files = get_list_of_pdfs_filenames(config.dpef_dir)
-    all_input_files = [input_file for input_file in all_input_files if
-                       input_file.name.split("_")[0] in companies_metadata_dict.keys()]
-    print(all_input_files)
+    all_dpef_paths = get_list_of_pdfs_filenames(config.dpef_dir)
+    all_dpef_paths = [dpef_path for dpef_path in all_dpef_paths if
+                       dpef_path.name.split("_")[0] in companies_metadata_dict.keys()]
+    print(all_dpef_paths)
 
     # PARALLELIZATION
     parallel_get_sentences_dataframe_from_pdf = partial(get_sentences_dataframe_from_pdf,
@@ -491,9 +491,9 @@ def get_sentences_from_all_pdfs(config):
             tqdm(
                 pool.imap(
                     parallel_get_sentences_dataframe_from_pdf,
-                    all_input_files
+                    all_dpef_paths
                 ),
-                total=len(all_input_files)
+                total=len(all_dpef_paths)
             )
         )
 
