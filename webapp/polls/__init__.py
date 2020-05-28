ca8fd9cb73108e28361193ccf262f459c7e3b6b6
Charles Gaydon <charles.gaydon@gmail.com>
2020-05-28 13:14:46 +0200

    Merged django commands to parse and model \n Tokenisation of words is done only once during parsing

--- webapp/polls/__init__.py
+++ webapp/polls/__init__.py
@@ -12,25 +12,10 @@ sys.path.append(str(dir_path / 'polls/rse_model'))
 model_directory = dir_path / "data/model"
 from rse_watch.conf import Config, DebugConfig
 from rse_watch.indexer import load_weighted_vectorizer
-config = Config(model_directory)
 
-# Load the model
-# N.B. model should be created prior to this,
-# USe 'python manage.py index_sentences' to do so.
-# TODO: Currently this is called even when populate_db is called and even
-#  before flush is called. This is
-#  a bit weird: parsing should be done once, and then the model created
-#  and then the model used by the server !
-#  --> This is not the right place for load_weighted_vectorizer.
-# documents = []
-# nlp = load_weighted_vectorizer(config,
-#                                documents,
-#                                create_from_scratch=False)
 nlp = None
 if 'runserver' in sys.argv:
+    config = Config(model_directory)
     nlp = load_weighted_vectorizer(config, None, create_from_scratch=False)
-# else:
-#     class nlp:
-#         def __init__(self, sentence):
-#             self.vector = np.random.random((300,))
+
 
