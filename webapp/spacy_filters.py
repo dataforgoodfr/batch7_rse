8e30aa5b9b6777291f5a14c09d533c3b8af4d36c
Hugo-GEE <hugo.muselli@gmail.com>
2020-06-01 19:21:08 +0200

    White word list improved

--- webapp/spacy_filters.py
+++ webapp/spacy_filters.py
@@ -1,37 +1,43 @@
 import pandas as pd
 
-
 white_list_words = pd.read_csv("./white_list_words.csv")['word'].values
 
+
+def condition(token):
+    result = (token.is_digit  # token must be a digit \
+              and len(token) == 4  # token must be of length 4
+              and int(token.text) > 0  # Token must be above certain date
+    )
+    return result
+
+
+# and tokens[token.i-1].shape[0]!=X
+# and tokens[token.i-1] in before_context.values
+
+BL = []
 
 def isDate(span, find_context=False, earliest=0):
     # type: (docs, Bool, int) -> object
     # We should be able to work this out with entities (any([t.label_ == "DATE" for t in doc.ents])
     dates = []
-    for token in span:
-        if (token.like_num
-                and token.is_digit  # token must be a digit \
-                and len(token) == 4  # token must be of length 4
-                and int(token.text) > earliest  # Token must be above certain date
-                and token.i != 0
-                and token.text in white_list_words
-                # and tokens[token.i-1].shape[0]!=X
-                # and tokens[token.i-1] in before_context.values
-
-        ):
+    for i, token in enumerate(span[0:]):
+        if condition(token):
+            """if span[i-1].text not in white_list_words:
+                BL.append(span[i-1].text)
+                return 'BL'"""
             current = []
             current.append((int(token.text)))
 
-            #This part is for debug
+            # This part is for debug
             if find_context == True:
                 if token.i != 0:
-                    before = tokens[token.i - 1]
+                    before = span[token.i - 1]
                 else:
                     before = ' '
                 """if token.i<len(tokens)-1:
                     after.append(tokens[token.i+1])"""
                 current.append(before)
-                #End of debug part
+                # End of debug part
 
             dates.append(current)
     result = [date[0] for date in dates] if dates else False
