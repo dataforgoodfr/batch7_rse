c2535e2160563cf79c4d06d541d06d3c4ef94ddc
Charles Gaydon <charles.gaydon@gmail.com>
2020-06-18 22:39:24 +0200

    Switched config for pg database

--- webapp/batch7rse/settings/dev.py
+++ webapp/batch7rse/settings/dev.py
@@ -1,4 +1,5 @@
 from batch7rse.settings.base import *
+from batch7rse.settings.credentials import DB_PASSWORD
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -9,12 +10,23 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'OPTIONS': {
+#             'timeout': 25, # in seconds
+#         },
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'OPTIONS': {
-            'timeout': 25, # in seconds
-        },
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dpef_db',
+        'USER': 'batch7rse',
+        'PASSWORD': DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '',  # default will be selected
     }
 }
