a30ef1c1666f8d64f63123eb9687c14d53789847
Charles Gaydon <charles.gaydon@gmail.com>
2020-06-27 20:20:08 +0200

    Evaluated memory usage

--- webapp/polls/__init__.py
+++ webapp/polls/__init__.py
@@ -13,14 +13,15 @@ from rse_watch.conf import Config, DebugConfig
 from rse_watch.indexer import run as run_indexer
 
 nlp = None
-# todo: revert when dev finished
-class nlp:
-    vector = np.random.random((300,))
-
-    def __init__(self, query):
-        pass
 
 if {'batch7rse.wsgi', 'runserver'}.intersection(sys.argv):
     # Load config, and will load existing vectorizer if already exists, else will create it.
     config = Config(model_directory)
-    # nlp = run_indexer(config)
+    nlp = run_indexer(config)
+
+# todo: revert when dev finished
+# class nlp:
+#     vector = np.random.random((300,))
+#
+#     def __init__(self, query):
+#         pass
