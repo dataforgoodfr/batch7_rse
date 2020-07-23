1efa4928f89ca403fb4473f19b7f5b7791bb411e
Charles Gaydon <charles.gaydon@gmail.com>
2020-07-23 15:52:06 +0200

    Corrected static root config

--- common/settings/base.py
+++ common/settings/base.py
@@ -113,5 +113,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_ROOT = './data/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# MEDIA_ROOT = './data/'
