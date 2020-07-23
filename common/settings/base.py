4dff4766ffc2ca3c525a125a2c150e0a993b5787
Charles Gaydon <charles.gaydon@gmail.com>
2020-07-23 16:07:38 +0200

    removed collection of static files

--- common/settings/base.py
+++ common/settings/base.py
@@ -114,5 +114,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
 # STATIC_ROOT = os.path.join(PROJECT_DIR, 'data')
