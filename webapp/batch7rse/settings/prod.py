5978be42f377fb6b293287988bf8340b29454bda
Charles Gaydon <charles.gaydon@gmail.com>
2020-07-23 11:05:32 +0200

    Updated prod setting to add db credentials

--- webapp/batch7rse/settings/prod.py
+++ webapp/batch7rse/settings/prod.py
@@ -1,11 +1,22 @@
 from batch7rse.settings.base import *
-import django_heroku
+from batch7rse.settings.credentials import DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEBUG' in os.environ
 
 # Activate Django-Heroku, this will set DATABASES['default'] and ALLOWED_HOSTS
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USERNAME,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,  # default will be selected
+    }
+}
