4776bf6098540cc20981c47f6599d658fc0590cd
Charles Gaydon <charles.gaydon@gmail.com>
2020-07-27 12:57:48 +0200

    added slash at end of static url

--- common/settings/base.py
+++ common/settings/base.py
@@ -117,6 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = "/static"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
 # STATIC_ROOT = os.path.join(PROJECT_DIR, 'data')
