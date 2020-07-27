1a0650954f7d4464d87dd5ed5a54a2a1d81e50ff
Charles Gaydon <charles.gaydon@gmail.com>
2020-07-27 11:03:34 +0200

    Fake model mode activated

--- polls/__init__.py
+++ polls/__init__.py
@@ -17,11 +17,11 @@ nlp = None
 if {'common.wsgi', 'runserver'}.intersection(sys.argv):
     # Load config, and will load existing vectorizer if already exists, else will create it.
     config = Config(model_directory)
-    nlp = run_indexer(config)  # todo : choose the right loading function, not run_indexer.
+    # nlp = run_indexer(config)  # todo : choose the right loading function, not run_indexer.
 
     # # todo: revert when dev finished
-    # class nlp:
-    #     vector = np.random.random((300,))
-    #
-    #     def __init__(self, query):
-    #         pass
+    class nlp:
+        vector = np.random.random((300,))
+
+        def __init__(self, query):
+            pass
