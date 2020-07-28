f5141721e729e320f6b3e51cbd7eb3c5101a7a84
Charles Gaydon <charles.gaydon@gmail.com>
2020-07-28 17:08:24 +0200

    Using load vectorizer and changing condition to use the real model

--- polls/__init__.py
+++ polls/__init__.py
@@ -10,15 +10,15 @@ dir_path = Path(os.getcwd())
 sys.path.append(str(dir_path / 'polls/rse_model'))
 model_directory = dir_path / "data/model"
 from rse_watch.conf import Config, DebugConfig
-from rse_watch.indexer import run as run_indexer
+from rse_watch.indexer import load_weighted_vectorizer
 
-nlp = None
+nlp = "a"
 
-if {'common.wsgi', 'runserver'}.intersection(sys.argv):
-    # Load config, and will load existing vectorizer if already exists, else will create it.
+if {'wsgi', 'runserver'}.intersection(sys.argv):
     config = Config(model_directory)
-    nlp = run_indexer(config)  # todo : choose the right loading function, not run_indexer.
-
+    nlp = load_weighted_vectorizer(config,
+                                 [],  # should be already created at deployment !
+                                 create_from_scratch=False)
     # # # todo: revert when dev finished
     # class nlp:
     #     vector = np.random.random((300,))
