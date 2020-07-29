f43c4f34e6c25b2465a3aed13f238ec844e736f8
Charles Gaydon <charles.gaydon@gmail.com>
2020-07-29 12:51:12 +0200

    Migration to Clever Cloud by refactorization

---
+++ common/settings/prod.py
@@ -0,0 +1,28 @@
+from common.settings.base import *
+try:
+    from common.settings.credentials import DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT
+except:
+    # env injection from clever cloud
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USERNAME = os.getenv("DB_USERNAME")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = 'DEBUG' in os.environ
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USERNAME,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,  # default will be selected
+    }
+}
---
+++ polls/__init__.py
@@ -0,0 +1,28 @@
+# TODO: maybe all this could be removed, and loading the model done in specific commands
+#  like in populate_db and
+import numpy as np
+import sys
+import os
+from pathlib import Path
+
+# Importing rse_watch package and config
+dir_path = Path(os.getcwd())
+sys.path.append(str(dir_path / 'polls/rse_model'))
+model_directory = dir_path / "data/model"
+from rse_watch.conf import Config, DebugConfig
+from rse_watch.indexer import load_weighted_vectorizer
+
+nlp = "a"
+
+print(sys.argv)
+if not {'collectstatic', 'populate_db'}.intersection(sys.argv):
+    config = Config(model_directory)
+    nlp = load_weighted_vectorizer(config,
+                                 [],  # should be created at deployment
+                                 create_from_scratch=False)
+    # todo: revert when dev finished
+    # class nlp:
+    #     vector = np.random.random((300,))
+    #
+    #     def __init__(self, query):
+    #         pass
